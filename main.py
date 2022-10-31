72b04f4404987c1634939e2c2b97b4f3e55a2af5
dgeison <paradg@gmail.com>
2022-10-30 21:57:23 -0300

    added prediction route

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
 import pickle
+import numpy as np
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Data(BaseModel):
+    baseline_value: float
+    accelerations: float
+    fetal_movement: float
+    uterine_contractions: float
+    light_decelerations: float
+    severe_decelerations: float
+    prolongued_decelerations: float
 
 
 def loadModels():
@@ -19,3 +31,19 @@ app = FastAPI()
 @app.get('/')
 def function():
     return {'nome': 'Dgeison Serrão Peixoto'}
+
+
+@app.post('/api/predict')
+def predict(request: Data):
+    received_data = np.array([
+        request.baseline_value,
+        request.accelerations,
+        request.fetal_movement,
+        request.uterine_contractions,
+        request.light_decelerations,
+        request.severe_decelerations,
+        request.prolongued_decelerations
+    ]).reshape(1, -1)
+    prediction = model.predict(scaler.transform(received_data))[0]
+    return {'y_pred': prediction}
+    
